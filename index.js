1aca0f26537ea690f8d0c9c11ba061ce702a961f
Dan Cunnington <dancunnington@uk.ibm.com>
2019-03-04 16:17:11 -0400

    ensure db is defined before the call to insert

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const wiot = require('./lib/watson_iot.js');
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
-// Save Postgres certificate to file
-const cert_b64 = process.env.PG_CERT_BASE_64
-const cert_buffer = new Buffer(cert_b64, 'base64');  
-const cert_ascii = cert_buffer.toString('ascii');
-
 // Watson IoT device type to subscribe too
 const device_types = process.env.WIOT_DEVICE_TYPES.split(',')
 
@@ -24,7 +19,7 @@ let insertWrapper = (deviceType, deviceId, eventType, format, payload) => {
   pg.insertEvent(deviceType, deviceId, eventType, format, payload, io)
 }
 
-pg.setup(cert_ascii).then(db => {
+pg.setup().then(db => {
   wiot.setup().then(ducks => {
     console.log('listening for device events...')
 
