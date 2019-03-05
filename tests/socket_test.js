9b405073c23ece09cf98818f43a3be937a0d6a73
Dan Cunnington <dancunnington@uk.ibm.com>
2019-03-05 16:42:01 -0400

    subscribe to both events in the socket test

--- tests/socket_test.js
+++ tests/socket_test.js
@@ -1,18 +1,22 @@
 const io = require('socket.io-client');
 
 // Local
-const socket_host = 'http://localhost:3000'
+//const socket_host = 'http://localhost:3000'
 
 // IBM Cloud
-//const socket_host = 'http://ducks-to-db.mybluemix.net'
+const socket_host = 'http://ducks-to-db.mybluemix.net'
 const socket = io(socket_host);
 
 socket.on('connect', function(){
   console.log('socket connected')
+  socket.on('civilian', function(data){
+    console.log(data)
+  });
+  socket.on('androidDebug', function(data){
+    console.log(data)
+  });
 });
-socket.on('androidDebug', function(data){
-  console.log(data)
-});
+
 socket.on('disconnect', function(){
   console.log('disconnected')
 });
