bfed278469e296ef341ba71c472cda848bc70495
Dan Cunnington <dancunnington@uk.ibm.com>
2019-03-07 18:37:57 -0400

    add error handling for passing no message ids on message status check

--- api/errors.js
+++ api/errors.js
@@ -1,5 +1,6 @@
 module.exports = {
   "no_device_type_and_id": "Please specify a device type and a device ID",
   "device_not_found": "Device not found",
-  "no_device_type": "Please specify a device type"
+  "no_device_type": "Please specify a device type",
+  "no_ids_error": "Please specify at least 1 message ID"
 }
